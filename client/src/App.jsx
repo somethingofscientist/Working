c29ee211d6068f3a3372f299a156df368ccad621
abhinav-siwal <abhinav.siwal@applore.in>
2023-08-20 15:42:17 +0530

    setup first files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar.jsx';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login';
+import Register from './pages/Register';
 function App() {
 
   return (
     <>
-      <h1>Sahil</h1>
+      <Navbar />
+      <Routes>
+        <Route  path='/' element={ <Home /> } />
+        <Route  path='/login' element={ <Login /> } />
+        <Route  path='/register' element={ <Register /> } />
+      </Routes>
     </>
   )
 }
