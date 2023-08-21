5946e983841cafcaf722454bd6514e5ab1821ca9
abhinav-siwal <abhinav.siwal@applore.in>
2023-08-21 13:24:14 +0530

    register page is working

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8000';
+// axios.defaults.withCredentials = true;
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter >
+      <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
