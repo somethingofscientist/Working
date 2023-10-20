556ad32afc4e2f5ee602b3584b82409060b239e2
somethingofscientist <1shraddhanand@gmail.com>
2023-10-20 11:59:33 +0530

    adding routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import PageNotFound from './components/pages/PageNotFound';
+import Register from './components/pages/Register';
+import Username from './components/pages/Username';
+import Password from './components/pages/Password';
+import Profile from './components/pages/Profile';
+import Reset from './components/pages/Reset';
+import Recovery from './components/pages/Recovery';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Username></Username>
+  },
+  {
+    path: '/register',
+    element: <Register></Register>
+  },
+  {
+    path: '/password',
+    element: <Password></Password>
+  },
+  {
+    path: '/profile',
+    element: <Profile></Profile>
+  },
+  {
+    path: '/reset',
+    element: <Reset></Reset>
+  },
+  {
+    path: '/recovery',
+    element: <Recovery></Recovery>
+  },
+  {
+    path: '/*',
+    element: <PageNotFound></PageNotFound>
+  }
+])
 
 function App() {
   return (
-    <div className="text-4xl text-cyan-400 font-bold">
-      Hello world!
-    </div>
+    <main>
+      <RouterProvider router={router}>
+
+      </RouterProvider>
+    </main>
   );
 }
 
